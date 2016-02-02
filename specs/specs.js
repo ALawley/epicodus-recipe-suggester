dbb077e2f5c7b7e77ed5079bbe111aaf69b54ab9
Anna Lawley and Kate Hall <alawley1@gmail.com>
2016-02-01 16:18:23 -0800

    create constructor for recipe objects

---
+++ specs/specs.js
@@ -0,0 +1,18 @@
+describe('Recipe', function() {
+  it("identifiy if a recipe has a user specified ingredient", function() {
+    var testRecipe = new Recipe("img.com", "Food", "Food.com", "Food.com/rosemary_chicken", "rosemary chicken", ["chicken", "rosemary", "butter"]);
+    expect(testRecipe.img_url).to.equal("img.com");
+    expect(testRecipe.publisher).to.equal("Food");
+    expect(testRecipe.publisher_url).to.equal("Food.com");
+    expect(testRecipe.source_url).to.equal("Food.com/rosemary_chicken");
+    expect(testRecipe.recipeTitle).to.equal("rosemary chicken");
+    expect(testRecipe.ingredient_list).to.eql(["chicken", "rosemary", "butter"]);
+  });
+});
+
+// describe('recipe', function() {
+//   it("identifiy if a recipe has a user specified ingredient", function() {
+//     var testrecipe = new recipe("chicken");
+//     expect(testrecipe.).to.equal(true);
+//   )};
+//   )};
