b11d22a94164438197cd8eb124e83460a58f80d6
Anna Lawley, Kate Hall and rob reasoner <alawley1@gmail.com>
2016-02-02 11:10:14 -0800

    add find recipe button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,11 @@ var BottomShelf = function(){
 BottomShelf.prototype.ingredientCheck = function(ingredient1, ingredient2) {
   if (ingredient1 === "Beef" && ingredient2 === "Zucchini") {
     return("teriyakiBeef");
-  } else if (ingredient1 === "Beef" && ingredient2 === "Mushroom") {
+  } else if (ingredient1 === "Beef" && ingredient2 === "Mushrooms") {
     return("stroganoff");
   } else if (ingredient1 === "Chicken" && ingredient2 === "Zucchini") {
     return("risotto");
-  } else if (ingredient1 === "Chicken" && ingredient2 === "Mushroom") {
+  } else if (ingredient1 === "Chicken" && ingredient2 === "Mushrooms") {
     return("teriyakiChicken");
   } else {
     return("ratatouille");
@@ -32,6 +32,7 @@ var Recipe = function(img_url, publisher, publisher_url, source_url, recipeTitle
 // newBottomShelf.recipeArray.push(stroganoff)
 
 $(document).ready(function() {
+  var newBottomShelf = new BottomShelf();
   //clicks on an li with a parent ul #ingredient 1
     //we take the value of that li
     //put the value into span class selected-ingredient
@@ -43,8 +44,12 @@ $(document).ready(function() {
       $('span#selected-ingredient2').text($(this).attr('value'));
     });
 
-    $('.ingredient3').click(function() {
-      $('span#selected-ingredient3').text($(this).attr('value'));
+    // $('.ingredient3').click(function() {
+    //   $('span#selected-ingredient3').text($(this).attr('value'));
+    // });
+    $("#find-recipes").click(function() {
+      newBottomShelf.ingredient1 = $('span#selected-ingredient1').text();
+      newBottomShelf.ingredient2 = $('span#selected-ingredient2').text();
+      alert(newBottomShelf.ingredientCheck(newBottomShelf.ingredient1, newBottomShelf.ingredient2));
     });
-
 });
