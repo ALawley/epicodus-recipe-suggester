1ec09a21e95fcb6b43d38060ffc91a50924d2b7f
Anna Lawley and Kate Hall <kwarrenhall@gmail.com>
2016-02-03 09:53:31 -0800

    update api request

--- js/back-end.js
+++ js/back-end.js
@@ -1,14 +1,20 @@
-var searchOuput = {};
+var recipeArray = [];
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://food2fork.com/api/search?key=1163698b40a362855b8b3cecccdd37f8&q=shredded%20chicken,spinach&sort=r", false);
-xhr.send();
-searchOutput = JSON.parse(xhr.responseText);
 var recipeIds = [];
-for (var i = 0; i < 5; i++) {
-  recipeIds.push(searchOutput.recipes[i].recipe_id);
+xhr.open("GET", "http://food2fork.com/api/search?key=1163698b40a362855b8b3cecccdd37f8&q=shredded%20chicken,spinach&sort=r", true);
+xhr.send();
+xhr.onloadend = function() {
+  console.log("hi1");
+  var searchOutput = JSON.parse(xhr.responseText);
+  console.log("hi2");
+  console.log(searchOutput);
+  for (var i = 0; i < 5; i++) {
+    recipeIds.push(searchOutput.recipes[i].recipe_id);
+  }
+  recipeIds.forEach(function(recipeId) {
+    xhr.open("GET", "http://food2fork.com/api/get?key=1163698b40a362855b8b3cecccdd37f8&rId=" + recipeId, true);
+    xhr.send();
+    recipeArray.push(xhr.response);
+    return recipeArray;
+  });
 }
-recipeIds.forEach(function(recipeId) {
-  xhr.open("GET", "http://food2fork.com/api/get?key=1163698b40a362855b8b3cecccdd37f8&rId=" + recipeId, false);
-  xhr.send();
-  console.log(xhr.responseText);
-});
