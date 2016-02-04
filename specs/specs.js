5c2a5b13526649e56a1dcd20d3830a0e7bec5bb2
Anna Lawley and Michelle Brecunier <alawley1@gmail.com>
2016-02-04 09:03:09 -0800

    update specs to use new recipes

--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,9 @@
-var teriyakiBeef = new Recipe("http://hostedmedia.reimanpub.com/TOH/Images/Photos/37/300x300/exps33521_SD955952D19.jpg", "Taste of Home", "tasteofhome.com",  "http://www.tasteofhome.com/recipes/teriyaki-zucchini-and-onion", "Teriyaki Beef", ["zucchini", "onion", "butter", "teriaki", "sesame seeds (optional)"]);
-var stroganoff = new Recipe("http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2012/11/12/0/FN_Paula-Deen-Beef-Stroganoff_s4x3.jpg.rend.sni12col.landscape.jpeg", "food network", "foodnetwork.com", "http://www.foodnetwork.com/recipes/paula-deen/beef-stroganoff-recipe.html", "Beef Stroganoff", ["beef", "flour", "olive oil", "butter", "onion", "mushrooms", "beef broth", "cream of mushroom", "pepper", "sour cream", "egg noodle"]);
-var risotto = new Recipe("http://cdn-image.myrecipes.com/sites/default/files/styles/300x300/public/image/recipes/sl/12/03/chicken-risotto-spring-vegetables-sl-x.jpg?itok=2yrT1ExR", "My Recipes", "myrecipes.com", "http://www.myrecipes.com/recipe/chicken-risotto-spring-vegetables", "Chicken Risotto with Spring Vegetables", ["chicken broth", "asparagus spears", "chicken breasts", "butter", "olive oil", "zucchini", "onion", "arborio rice", "white wine", "parmesan cheese", "fontina cheese", "parsley"]);
-var teriyakiChicken = new Recipe("http://images.edge-generalmills.com/de3c686e-05e1-4cf6-9b36-cd604b9703a2.jpg", "Betty Crocker", "bettycrocker.com", "http://www.bettycrocker.com/recipes/teriyaki-chicken-stir-fry/87b85076-efa5-4acf-90e0-f84fd5bd3353", "Teriaki Chicken", ["chicken breast", "teriaki sauce", "lemon juice", "bag of frozen broccoli, carrots, waterchestnuts and redpeppers", "a starch (ie. rice, couscous, noodles"]);
-var ratatouille = new Recipe("http://www.cdkitchen.com/recipes/images/2013/08/55326-3119-mx.jpg", "cd Kitchen", "cdkitchen.com", "http://www.cdkitchen.com/recipes/recs/1551/Mushroom-Ratatouille84542.shtmlf", "Ratatouille", ["mushrooms", "onion", "eggplant", "zucchini", "red bell pepper", "tomato", "olive oil", "garlic"]);
+var kebabs = new Recipe("http://static.food2fork.com/30716_RecipeImage_620x413_beef_vegetable_shish_kebabs_2dc9e.jpg", "Chow", "http://www.chow.com",  "http://www.chow.com/recipes/30716-beef-and-vegetable-shish-kebabs", "Beef and Vegetable Shish Kebabs", ["1 tablespoon kosher salt", "2 teaspoons packed light brown sugar", "1 teaspoon finely grated lemon zest (from about 1 medium lemon)", "1/2 teaspoon ground cumin", "1/2 teaspoon paprika", "1/4 teaspoon freshly ground black pepper", "1/4 teaspoon ground coriander", "1/3 cup olive oil", "2 tablespoons tomato paste", "5 medium garlic cloves, finely chopped", "1 (1-1/2 to 2-pound) sirloin steak, about 1 inch thick", "8 ounces cremini mushrooms", "1 pound zucchini", "1 medium red onion", "10 (12-inch) wooden or metal skewers"]);
+var bourguignonne = new Recipe("http://static.food2fork.com/beefdish3_300269b1efe.jpg", "Real Simple", "http://realsimple.com/", "http://www.realsimple.com/food-recipes/browse-all-recipes/quick-beef-bourguignonne-10000001699374/index.html", "Quick Beef Bourguignonne", ["1 tablespoon olive oil", "1 1/4 pounds sirloin steak, cut into 1-inch pieces", "1 kosher salt and pepper", "1 10-ounce package sliced mushrooms", "2 cups 16-ounce package frozen pearl onions", "1 red wine", "1/2 cup 10.75-ounce can Campbells Golden Mushroom soup\n"]);
+var chickenPappardelle = new Recipe("http://static.food2fork.com/354130a3e3.jpg", "Epicurious", "http://www.epicurious.com/", "http://www.epicurious.com/recipes/food/views/Chicken-Breasts-with-Zucchini-Pappardelle-354130", "Chicken Breasts with Zucchini Pappardelle", ["1 pound zucchini, trimmed", "2 garlic cloves", "4 boneless chicken breast halves with skin (1 1/2 pounds) ", "1 tablespoon olive oil", "2 tablespoons water", "1 cup torn basil leaves"]);
+var chickenMarsala = new Recipe("http://static.food2fork.com/47653a1af.jpg", "All Recipes", "http://allrecipes.com/", "http://allrecipes.com/Recipe/Chicken-Marsala/Detail.aspx", "Chicken Marsala", ["1/4 cup all-purpose flour for coating", "1/2 teaspoon salt", "1/4 teaspoon ground black pepper", "1/2 teaspoon dried oregano", "4 skinless, boneless chicken breast halves - pounded 1/4 inch thick", "4 tablespoons butter", "4 tablespoons olive oil", "1 cup sliced mushrooms", "1/2 cup Marsala wine", "1/4 cup cooking sherry"]);
+var zucchiniBalls = new Recipe("http://static.food2fork.com/Kolokythokeftedes(ZucchiniandFetaBalls)150043120830.jpg", "Closet Cooking", "http://closetcooking.com/", "http://www.closetcooking.com/2009/09/kolokythokeftedes-zucchini-and-feta.html", "Kolokythokeftedes (Zucchini and Feta Balls)", ["2 cups zucchini, grated and squeezed", "1/4 cup of feta, crumbled", "1 splash ouzo (optional)", "1 clove garlic, chopped", "2 green onions, sliced", "1 handful herbs (such as mint, dill and parsley), chopped", "1 egg, lightly beaten", "salt and pepper to taste", "flour (I used whole wheat)", "breadcrumbs (I use whole wheat)", "oil for frying\n"]);
+var mushroomSalad = new Recipe("http://static.food2fork.com/Warm2BMushroom2Band2BRoasted2BAsparagus2BWild2BRice2BSalad2Bwith2Band2BFeta2B5002B71693724de9b.jpg", "Closet Cooking", "http://closetcooking.com/", "http://www.closetcooking.com/2011/06/warm-mushroom-and-wild-rice-salad-with.html", "Warm Mushroom, Roasted Asparagus and Wild Rice Salad with Feta", ["1/2 cup wild rice", "1 1/4 cups broth, chicken or vegetable", "1 tablespoon oil", "1 onion, diced", "2 cloves garlic, chopped", "8 ounces mushrooms, cleaned and sliced", "salt and pepper to taste", "1 tablespoon oil", "salt and pepper to taste", "1 pound asparagus, trimmed", "1 handful dill, chopped", "1/4 cup balsamic vinaigrette", "4 cups salad greens", "1/4 cup feta or goat cheese or blue cheese, crumbled\n"]);
 
 describe('BottomShelf', function() {
   it("hold all page information", function() {
@@ -12,15 +13,15 @@ describe('BottomShelf', function() {
     expect(testBottomShelf.chosenRecipe).to.eql({});
     expect(testBottomShelf.recipeArray).to.eql([]);
   });
-  it("will run ingredient check and return ratatouille", function() {
+  it("has a prototype to check the ingredient selects", function() {
     var testBottomShelf = new BottomShelf();
-    testBottomShelf.recipeArray = [teriyakiBeef, stroganoff, risotto, teriyakiChicken, ratatouille];
-    expect(testBottomShelf.ingredientCheck()).to.equal(ratatouille);
+    expect(testBottomShelf.ingredientCheck("Select", "Select")).to.equal(false);
   });
-  it("will run ingredient check and return teriyakiBeef if ingredients are beef and zucchini", function() {
+  it("will return recipe using the two ingredients", function() {
     var testBottomShelf = new BottomShelf();
-    testBottomShelf.recipeArray = [teriyakiBeef, stroganoff, risotto, teriyakiChicken, ratatouille];
-    expect(testBottomShelf.ingredientCheck("Beef", "Zucchini")).to.equal(teriyakiBeef);
+    testBottomShelf.recipeArray = [kebabs, bourguignonne, chickenPappardelle, chickenMarsala, zucchiniBalls, mushroomSalad];
+    expect(testBottomShelf.ingredientCheck("Beef", "Zucchini")).to.equal(kebabs);
+    expect(testBottomShelf.ingredientCheck("Chicken", "Mushrooms")).to.equal(chickenMarsala);
   });
 });
 
